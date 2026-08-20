47fa8b4931a29ece4a6ff5a556f0338a7e55088b
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-08-20 22:13:14 +0600

    feat: add allowed development origins to configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "points-devoted-incorporated-whose.trycloudflare.com",
+  ],
 };
 
 export default nextConfig;
