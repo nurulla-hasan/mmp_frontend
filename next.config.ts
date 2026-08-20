b44b16dd67e9654805038dca1b3dcfaa39d04cd6
Nurulla Hasan <85323050+nurulla-hasan@users.noreply.github.com>
2026-08-20 23:16:29 +0600

    chore: remove temporary tunnel dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: [
-    "points-devoted-incorporated-whose.trycloudflare.com",
-  ],
 };
 
 export default nextConfig;
